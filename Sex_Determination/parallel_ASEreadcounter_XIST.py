00860f4d0a247b26e6c7c1795127e1c5c3345814
as1000 <32502256+as1000@users.noreply.github.com>
2022-03-06 20:08:34 -0500

    Update parallel_ASEreadcounter_XIST.py

--- Sex_Determination/parallel_ASEreadcounter_XIST.py
+++ Sex_Determination/parallel_ASEreadcounter_XIST.py
@@ -12,7 +12,7 @@ val = min(task_id*samples_per_node, len(parameters_list))
 parameter_sublist = parameters_list[(task_id - 1)*samples_per_node:val]
 
 def runSimulation(parameter):
-        os.system("java -Djava.io.tmpdir=xist_aser -Xmx8000m -jar gatk.jar ASEReadCounter -R Homo_sapiens_assembly38.fasta --read-filter PassesVendorQualityCheckReadFilter --read-filter HasReadGroupReadFilter  --read-filter NotDuplicateReadFilter --read-filter MappingQualityAvailableReadFilter --read-filter NotSecondaryAlignmentReadFilter --read-filter MappingQualityReadFilter --minimum-mapping-quality 30 --read-filter OverclippedReadFilter --filter-too-short 25 --read-filter GoodCigarReadFilter --read-filter AmbiguousBaseReadFilter -V hapmap_3.3.hg38.vcf.gz --lenient --seconds-between-progress-updates 100 -I download_dir/%s/normal.bam -L chrX -O output_dir/%s.out" % (parameter,parameter))
+        os.system("java -Djava.io.tmpdir=xist_aser -Xmx8000m -jar gatk.jar ASEReadCounter -R Homo_sapiens_assembly38.fasta --read-filter PassesVendorQualityCheckReadFilter --read-filter HasReadGroupReadFilter  --read-filter NotDuplicateReadFilter --read-filter MappingQualityAvailableReadFilter --read-filter NotSecondaryAlignmentReadFilter --read-filter MappingQualityReadFilter --minimum-mapping-quality 30 --read-filter OverclippedReadFilter --filter-too-short 25 --read-filter GoodCigarReadFilter --read-filter AmbiguousBaseReadFilter -V hapmap_3.3.hg38.vcf.gz --lenient --seconds-between-progress-updates 100 -I $temp_path/%s/normal.bam -L chrX -O output_dir/%s.out" % (parameter,parameter))
 
 for parameter in parameter_sublist:
         runSimulation(parameter = parameter)
